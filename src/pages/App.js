11dfef3ac0b5ff5ab37f589757e76d06871ac3f6
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-08-23 16:06:25 -0700

    add routing options to app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import Header from '../components/header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <p>home</p>
+          </Route>
+          <Route exact path="/aboutme">
+            <p>about</p>
+          </Route>
+          <Route exact path="/projects">
+            <p>projects</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
