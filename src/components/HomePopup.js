c3a2437776eac7bc8e5382424cc75c6a683e0ed5
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-08-24 15:00:25 -0700

    amend styling

---
+++ src/components/HomePopup.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import "../styles/components/HomePopup.css"
+
+export default function HomePopup() {
+  
+  return (
+    <div className="popupFrame" >
+      <p>text</p>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Background from '../components/Background'
+import HomePopup from '../components/HomePopup'
 import "../styles/pages/Home.css"
 
 export default function Home({}) {
   return (
     <div>
       <Background />
+      <HomePopup />
     </div>
   )
 }
