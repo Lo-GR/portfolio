1e8a13a190a84d4f5aeac9850663105a1656d94c
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-08-31 15:45:22 -0700

    add filler and refactor css h2 to global

--- src/components/HomePopup.js
+++ src/components/HomePopup.js
@@ -15,7 +15,8 @@ export default function HomePopup() {
           </div>
         :
           <div className="popupFrame" >
-            <p>text</p>
+            <h2>WELCOME</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus vehicula, placerat mauris nec, congue nisi. Donec pretium orci at lacus sodales mollis. Aliquam erat volutpat. Mauris viverra semper nunc, at pharetra odio laoreet eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi efficitur, diam sit amet ultrices scelerisque, quam enim eleifend ipsum, sit amet sollicitudin dui metus at turpis. Suspendisse potenti. Aenean varius imperdiet malesuada. Quisque ante ante, ultrices sit amet orci vitae, ornare mollis lectus. Aliquam nulla turpis, imperdiet convallis est ut, euismod scelerisque eros. Nullam dapibus scelerisque ultrices. Nulla bibendum risus id mollis semper. Nam vulputate quam semper odio ullamcorper cursus. Nunc porta lectus a erat malesuada, at ornare metus mattis.</p>
           </div>
         }
       </div>
