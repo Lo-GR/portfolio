7a1d710a19474b47a1955744b6095a49485d62a1
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-08-31 13:25:15 -0700

    styling and logic for entering

--- src/components/HomePopup.js
+++ src/components/HomePopup.js
@@ -2,16 +2,22 @@ import React, {useState, useEffect} from 'react'
 import "../styles/components/HomePopup.css"
 
 export default function HomePopup() {
-  
+  const [ enter, setEnter ] = useState(false)
+  const handleEnter = () =>{
+    setEnter(true);
+  }
   return (
     <div>
-      <div className="mainArea">
-        <div className="pressableEnter">
-          <h2>E N T E R</h2>
-        </div>
-        {/* <div className="popupFrame" >
-          <p>text</p>
-        </div> */}
+      <div className="mainArea" >
+        {!enter ? 
+          <div className="pressableEnter" onClick={handleEnter}>
+            <h2>E N T E R</h2>
+          </div>
+        :
+          <div className="popupFrame" >
+            <p>text</p>
+          </div>
+        }
       </div>
     </div>
   )
