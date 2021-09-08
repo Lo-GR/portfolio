18bf3af1e89b1a933e029dd2a02b23680271d8d4
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-07 19:30:33 -0700

    amend styling and add additional links templates

--- src/components/HomepageLinks.js
+++ src/components/HomepageLinks.js
@@ -4,9 +4,26 @@ import "../styles/components/HomepageLinks.css"
 export default function HomepageLinks() {
   return (
     <div className="homepageArea">
-      <div className="backPart">
-        <div className="frontPart">
-          <h2>&lt;/&gt;</h2>
+      <div className="linkArea">
+        <div className="backPart">
+          <div className="frontPart">
+            <h2>&lt;/&gt;</h2>
+          </div>
+          <h2 className="homepageText">Coding Projects</h2>
+        </div>
+      </div>
+      <div className="linkArea">
+        <div className="backPart">
+          <div className="frontPart">
+            <h2>ME</h2>
+          </div>
+        </div>
+      </div>
+      <div className="linkArea">
+        <div className="backPart">
+          <div className="frontPart">
+            <h2>?!</h2>
+          </div>
         </div>
       </div>
     </div>
