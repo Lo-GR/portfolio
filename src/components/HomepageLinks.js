fa95eb8bb03a41f85fb47a6e1888a43db6fa974b
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-13 15:00:58 -0700

    connect links to homepagelinks

--- src/components/HomepageLinks.js
+++ src/components/HomepageLinks.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import "../styles/components/HomepageLinks.css"
+import { Link } from "react-router-dom";
 
 export default function HomepageLinks() {
   return (
     <div className="homepageArea">
-      <div className="linkArea">
-        <div className="backPart">
-          <div className="frontPart">
-            <h2>&lt;/&gt;</h2>
+      <Link to="/projects" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+        <div className="linkArea">
+          <div className="backPart">
+            <div className="frontPart">
+              <h2>&lt;/&gt;</h2>
+            </div>
+            <h2 className="homepageText">Coding Projects</h2>
           </div>
-          <h2 className="homepageText">Coding Projects</h2>
         </div>
-      </div>
-      <div className="linkArea">
-        <div className="backPart">
-          <div className="frontPart">
-            <h2>ME</h2>
+      </Link>
+      <Link to="/aboutme" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+        <div className="linkArea">
+          <div className="backPart">
+            <div className="frontPart">
+              <h2>ME</h2>
+            </div>
+            <h2 className="homepageText">About Me</h2>
           </div>
-          <h2 className="homepageText">About Me</h2>
         </div>
-      </div>
+      </Link>
       <div className="linkArea">
         <div className="backPart">
           <div className="frontPart">
