7eecb7f3d29f3da675ba69183e56bc701035e2f8
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-14 09:21:12 -0700

    remove links temporarily while page under construction

--- src/components/HomepageLinks.js
+++ src/components/HomepageLinks.js
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 export default function HomepageLinks() {
   return (
     <div className="homepageArea">
-      <Link to="/projects" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+      {/* <Link to="/projects" style={{ textDecoration: 'inherit', color: 'inherit' }}> */}
         <div className="linkArea">
           <div className="backPart">
             <div className="frontPart">
               <h2>&lt;/&gt;</h2>
             </div>
-            <h2 className="homepageText">Coding Projects</h2>
+            <h2 className="homepageText">Coding Projects (coming soon)</h2>
           </div>
         </div>
-      </Link>
-      <Link to="/aboutme" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+      {/* </Link> */}
+      {/* <Link to="/aboutme" style={{ textDecoration: 'inherit', color: 'inherit' }}> */}
         <div className="linkArea">
           <div className="backPart">
             <div className="frontPart">
               <h2>ME</h2>
             </div>
-            <h2 className="homepageText">About Me</h2>
+            <h2 className="homepageText">About Me (coming soon)</h2>
           </div>
         </div>
-      </Link>
+      {/* </Link> */}
       <div className="linkArea">
         <div className="backPart">
           <div className="frontPart">
