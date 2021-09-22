6c7100515166967a84f9b1ea30b7a6977b2cfc66
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-22 14:19:54 -0700

    add state for background animation trigger

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../../styles/components/Background.css"
+import ThreeFiber from './ThreeFiber'
 
 export default function Background({ animatedBackground }) {
-
   return (
     <div className="area" >
-      
+      {animatedBackground ? <ThreeFiber /> : null}
     </div>
   )
 }
