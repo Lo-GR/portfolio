06c6caf1ac86e7ea8275fbf1d015156f58ffb1db
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-28 14:10:12 -0700

    add additional styling

--- src/components/HomePopup.js
+++ src/components/HomePopup.js
@@ -43,7 +43,7 @@ export default function HomePopup({ setAppear }) {
                   </a>
                 </div>
               </div>
-              <div className="contentText" style={{animationDuration: "2s"}}>
+              <div className="contentText" style={{animationDuration: "3s"}}>
                 <div className="briefAboutMe">
                   <div className="briefChild">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus vehicula, placerat mauris nec, congue nisi. Donec pretium orci at lacus sodales mollis. Aliquam erat volutpat. Mauris viverra semper nunc, at pharetra odio laoreet eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi efficitur, diam sit amet ultrices scelerisque, quam enim eleifend ipsum, sit amet sollicitudin dui metus at turpis. Suspendisse potenti. Aenean varius imperdiet malesuada. Quisque ante ante, ultrices sit amet orci vitae, ornare mollis lectus. Aliquam nulla turpis, imperdiet convallis est ut, euismod scelerisque eros. Nullam dapibus scelerisque ultrices. Nulla bibendum risus id mollis semper. Nam vulputate quam semper odio ullamcorper cursus. Nunc porta lectus a erat malesuada, at ornare metus mattis.</p>
@@ -55,8 +55,9 @@ export default function HomePopup({ setAppear }) {
                 </div>
               </div>
             </div>
-            <div className="more" style={{animationDuration: "2s"}}>
-              <h3>Test</h3>
+            <div className="more" style={{animationDuration: "3.9s"}}>
+              <h3>Scroll down for links to additional information on myself, my projects and my personal blog!</h3>
+              <p></p>
               <img className="icons" src={Arrow} />
             </div>
           </div>
