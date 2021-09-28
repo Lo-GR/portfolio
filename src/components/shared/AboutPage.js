c12adf2159c94b334f176de6e96b650bee352fba
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-28 15:21:25 -0700

    add logic for showing div

--- src/components/shared/AboutPage.js
+++ src/components/shared/AboutPage.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import "../../styles/components/AboutPage.css"
 
 export default function AboutPage() {
-
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  }
   return (
     <div>
-      <div className="clickHere">
+      <div className="clickHere" onClick={handleClick}>
         <h2 className="mark">?</h2>
       </div>
+      {clicked ? <div> Big Testy</div> : null}
     </div>
   )
 }
