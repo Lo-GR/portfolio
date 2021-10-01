896148d1535a993e0068f0c55b0689c42b16d1e0
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-01 14:47:32 -0700

    create footer component content

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import "../../styles/components/Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className="footerArea">
+      <div className="footerContent">
+        <div className="personalInfo">
+          <p>Lo-GR</p>
+          <p>Logan Roth</p>
+          <p>Redmond, WA</p>
+        </div>
+        <div className="additionalLinks">
+          <p>Lo-GR</p>
+          <p>Logan Roth</p>
+          <p>Redmond, WA</p>
+        </div>
+      </div>
+      <div className="sectionDiver" />
+      <div className="copyRight">
+        <p>©2021 Logan Roth. License <a href="https://opensource.org/licenses/MIT" target="_blank">MIT</a></p>
+      </div>
     </div>
   )
 }
