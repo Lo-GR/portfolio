5618a8bb02969e096a8abc0b7bb99125f85b4a4d
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 15:31:10 -0700

    import background to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Footer from "../components/shared/Footer"
 import Header from '../components/shared/Header'
+import Background from "../components/Background/Background"
 
 export default function Projects() {
   return (
     <div>
       <Header />
       <Footer />
+      <Background />
     </div>
   )
 }
