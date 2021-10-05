586c6f9e83e0e57707f494f0f5f7f058697c2093
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 16:15:19 -0700

    create aboutmecontent component

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Footer from "../components/shared/Footer"
+import Header from '../components/shared/Header'
+import AboutMeContent from '../components/AboutMeContent'
 
 export default function AboutMe() {
   return (
     <div>
-      
+      <Header />
+      <ProjectsContent />
+      <Footer />
     </div>
   )
 }
