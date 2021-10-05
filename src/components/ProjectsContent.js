ee41cfea90829404784dbd6e32e9efe49e9ff799
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 15:36:13 -0700

    create projects component

---
+++ src/components/ProjectsContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProjectsContent() {
+  return (
+    <div className="projectsContentArea">
+      <h3>At the moment, this page is under construction. In the meantime, you can view my projects at <a href="https://github.com/Lo-GR?tab=repositories" target="_blank"> my GitHub</a></h3>
+    </div>
+  )
+}
