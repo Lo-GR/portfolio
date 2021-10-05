77b1cb90a938133cc20efb91cd3a074a56baf18b
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 16:16:05 -0700

    create aboutme css

---
+++ src/components/AboutMeContent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/components/AboutMeContent.css"
+
+export default function ProjectsContent() {
+  return (
+    <div className="aboutContentArea">
+      <p>At the moment, this page is under construction. In the meantime, you can view my projects at <a href="https://github.com/Lo-GR?tab=repositories" target="_blank"> my GitHub</a></p>
+    </div>
+  )
+}
