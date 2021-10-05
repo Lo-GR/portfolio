5b0c874ea7fc977c476e365a322a1fd6588fda69
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 15:49:15 -0700

    create temporary projects content page

--- src/components/ProjectsContent.js
+++ src/components/ProjectsContent.js
@@ -4,7 +4,7 @@ import "../styles/components/ProjectsContent.css"
 export default function ProjectsContent() {
   return (
     <div className="projectsContentArea">
-      <h3>At the moment, this page is under construction. In the meantime, you can view my projects at <a href="https://github.com/Lo-GR?tab=repositories" target="_blank"> my GitHub</a></h3>
+      <p>At the moment, this page is under construction. In the meantime, you can view my projects at <a href="https://github.com/Lo-GR?tab=repositories" target="_blank"> my GitHub</a></p>
     </div>
   )
 }
