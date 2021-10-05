2f1e41820dfce5be13ca1024dfaceee78c51b8dd
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-05 15:46:59 -0700

    add container and top part of header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -4,33 +4,36 @@ import "../../styles/components/Header.css";
 
 export default function Header({}) {
   return (
-    <div className="headerArea">
-      <div className="linkBox">
-        <h2>Logan</h2>
-      </div>
-      <div className="headerDivider">
-        <div className="divide"/>
-      </div>
-      <div className="linkBox">
-        <h3>Home</h3>
-      </div>
-      <div className="headerDivider">
-        <div className="divide"/>
-      </div>
-      <div className="linkBox">
-        <h3>Projects</h3>
-      </div>
-      <div className="headerDivider">
-        <div className="divide"/>
-      </div>
-      <div className="linkBox">
-        <h3>About Me</h3>
-      </div>
-      <div className="headerDivider">
-        <div className="divide"/>
-      </div>
-      <div className="linkBox">
-        <h3>Blog (Coming soon)</h3>
+    <div className="container">
+      <div className="top" />
+      <div className="headerArea">
+        <div className="linkBox">
+          <h2>Logan</h2>
+        </div>
+        <div className="headerDivider">
+          <div className="divide"/>
+        </div>
+        <div className="linkBox">
+          <h3>Home</h3>
+        </div>
+        <div className="headerDivider">
+          <div className="divide"/>
+        </div>
+        <div className="linkBox">
+          <h3>Projects</h3>
+        </div>
+        <div className="headerDivider">
+          <div className="divide"/>
+        </div>
+        <div className="linkBox">
+          <h3>About Me</h3>
+        </div>
+        <div className="headerDivider">
+          <div className="divide"/>
+        </div>
+        <div className="linkBox">
+          <h3>Blog (Coming soon)</h3>
+        </div>
       </div>
     </div>
   )
