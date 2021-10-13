e7134a1e0569acf3b358e83465930d3eedf65e45
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-13 14:18:33 -0700

    add filler content and disable scroll snap on smaller res

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -7,7 +7,11 @@ export default function ProjectDetails() {
     <div className="projectDetailsArea">
       <img src={Site} className="websiteImage"/>
       <div className="details">
-        
+        <h2>Memory Space</h2>
+        <h3>Description</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla posuere sollicitudin aliquam ultrices sagittis orci. In vitae turpis massa sed elementum tempus egestas. Interdum velit laoreet id donec ultrices tincidunt arcu. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Etiam sit amet nisl purus in mollis. Eget felis eget nunc lobortis m</p>
+        <h3>Tech</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
       </div>
     </div>
   )
