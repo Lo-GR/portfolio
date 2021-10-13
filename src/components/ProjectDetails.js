ecdf95e7a6dfc9187c83f5cdfb80f5f1384a6c8a
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-13 16:03:27 -0700

    add hyperlinks to project details

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Site from "../assets/img/Memory-Space.png"
 import "../styles/components/ProjectDetails.css";
+import Arrow from "../assets/svg/arrow-circle-right.svg"
 
 export default function ProjectDetails({ siteImage, siteTitle, siteDesc, siteTech}) {
+  const siteLink = "https://www.lo-gr.com/"
   return (
     <div className="projectDetailsArea">
       <img src={siteImage} className="websiteImage"/>
       <div className="details">
-        <h2>{siteTitle}</h2>
+        <a href={siteLink} target="_blank">
+          <div className="linkSpace">
+            <h2>{siteTitle}</h2>
+            <img src={Arrow} />
+          </div>
+        </a>
         <h3>Description</h3>
         <p>{siteDesc}</p>
         <h3>Tech</h3>
